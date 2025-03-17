2f2d35e07ca2b7b575b14c110c2fd29ac527b9db
Viacheslav Lorai <lorayhunt@gmail.com>
2025-03-17 18:17:20 +1000

    Added navigation bar

---
+++ app/routes/about.tsx
@@ -0,0 +1,12 @@
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "О нас" },
+    { name: "description", content: "Информация о нас" },
+  ];
+}
+
+export default function Home() {
+  return <h1>О нас</h1>;
+}
